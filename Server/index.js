774821da40115056fca0f328488c58895fd24b0f
Vishal Karande <35804606+vishalkarande9@users.noreply.github.com>
2021-06-05 17:24:11 -0400

    Added Server Folder

---
+++ Server/index.js
@@ -0,0 +1,16 @@
+const app = require('express')()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+
+io.on('connection', socket => {
+  console.log("Client Connected!");
+
+  socket.on('message', ({ name, message }) => {
+    console.log("Message reveived on server!");
+    io.emit('message', { name, message })
+  })
+})
+
+http.listen(5000, function() {
+  console.log('listening on port 5000')
+})
